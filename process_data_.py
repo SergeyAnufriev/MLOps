ff0958036c789723f934939526980ea7941d3000
Sergey Anufriev <anufriiev.sergii@gmail.com>
2022-04-16 12:41:13 +0100

    upload data processing module

---
+++ process_data_.py
@@ -0,0 +1,45 @@
+'''This modules creates pytorch datasets for train/evel/test '''
+
+from torchtext.data.utils import get_tokenizer
+from torchtext.vocab import build_vocab_from_iterator
+from torch.utils.data.dataset import random_split
+from torchtext.data.functional import to_map_style_dataset
+import torch
+
+from torchtext.datasets import AG_NEWS
+
+
+tokenizer = get_tokenizer('basic_english')
+train_iter,test_iter = AG_NEWS()
+
+def yield_tokens(data_iter):
+    for _, text in data_iter:
+        yield tokenizer(text)
+
+vocab = build_vocab_from_iterator(yield_tokens(train_iter), specials=["<unk>"])
+vocab.set_default_index(vocab["<unk>"])
+
+'''functions which assign each word its unique number by given vocablury'''
+text_pipeline = lambda x: vocab(tokenizer(x))
+label_pipeline = lambda x: int(x) - 1
+
+'''function to convert list of tuples [(label,'hello world',...()] into batches'''
+def collate_batch(batch):
+    label_list, text_list, offsets = [], [], [0]
+    for (_label, _text) in batch:
+         label_list.append(label_pipeline(_label))
+         processed_text = torch.tensor(text_pipeline(_text), dtype=torch.int64)
+         text_list.append(processed_text)
+         offsets.append(processed_text.size(0))
+    label_list = torch.tensor(label_list, dtype=torch.int64)
+    offsets = torch.tensor(offsets[:-1]).cumsum(dim=0)
+    text_list = torch.cat(text_list)
+    return label_list, text_list, offsets
+
+'''Create datasets for train/test/eval'''
+train_dataset = to_map_style_dataset(train_iter)
+test_dataset = to_map_style_dataset(test_iter)
+num_train = int(len(train_dataset) * 0.95)
+split_train_, split_valid_ = \
+    random_split(train_dataset, [num_train, len(train_dataset) - num_train])
+
